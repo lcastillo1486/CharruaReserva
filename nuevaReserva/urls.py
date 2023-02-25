b416de52b5596463ab069115ab7511962ac2be8d
lcastillo1486 <126116225+lcastillo1486@users.noreply.github.com>
2023-02-24 20:19:05 -0500

    UltimosCambios

--- nuevaReserva/urls.py
+++ nuevaReserva/urls.py
@@ -6,11 +6,13 @@ urlpatterns = [
     path('enEspera/', views.listadoEnEspera, name ='enespera'),
     #path('enProceso/', views.listadoEnProceso),
     path('historico/', views.listadoCompletado),
-    path('reservasDelDia/', views.listadoDelDia),
+    path('reservasDelDia/', views.listadoDelDia, name='delDia'),
     path('editarReserva/<int:id>', views.editarReserva),
     path('cambiarEstadoReserva/<int:id>', views.cambioEstadoReserva),
     #path('cargaAsignacionMesa/<int:id>', views.cargaAsignarMesa),
     path('cambiarEstadoAnulado/<int:id>', views.cambiaEstadoAnulado),
     path('cambiarEstadoShow/<int:id>', views.cambiaEstadoNoShow),
     path('estadoActual/', views.estadoActual),
+    path('guardarMesa/<int:id>', views.guardarMesa),
+    
 ]
