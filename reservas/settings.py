72058a0e2b041e6ee96117c0ad9b8f66eea4d7b5
lcastillo1486 <126116225+lcastillo1486@users.noreply.github.com>
2023-03-01 14:36:37 -0500

    Cambio05

--- reservas/settings.py
+++ reservas/settings.py
@@ -85,9 +85,10 @@ WSGI_APPLICATION = 'reservas.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-         'default': dj_database_url.config(default='sqlite:////db.sqlite3')
-        
-    
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
