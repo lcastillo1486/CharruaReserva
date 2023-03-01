fe6d4983eb66093a0e52f973a73be048122f1f18
lcastillo1486 <126116225+lcastillo1486@users.noreply.github.com>
2023-03-01 12:53:51 -0500

    cambio02

--- reservas/settings.py
+++ reservas/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'reservas.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-         'default': dj_database_url.config(default='postgres://sistema_1fx2_user:p2wZgjmDhiHXen8ZNdyAYRJlhEC3jvFu@dpg-cfvn6pvdvk4rro7djs80-a/sistema_1fx2',        conn_max_age=600    )
+         'default': dj_database_url.config(default='postgres://reservas_user:Guu05MFydExtqFVXU6RFIxhudyU4HNAU@dpg-cfvp16vdvk4rro7tldd0-a/reservas',        conn_max_age=600    )
         
     
 }
