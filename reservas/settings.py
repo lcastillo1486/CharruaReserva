54260d32f5a060f326465b69c94444bff52259e4
lcastillo1486 <126116225+lcastillo1486@users.noreply.github.com>
2023-03-01 20:41:39 -0500

    Prueba65

--- reservas/settings.py
+++ reservas/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'reservas.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases 
 
 DATABASES = {
      'default': dj_database_url.config( default='postgresql://postgres:postgres@localhost:5432/mysite',conn_max_age=600    )
