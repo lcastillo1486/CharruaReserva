d2f9c0826b49dd8f1122bd6818ae4d103cc5b2d3
lcastillo1486 <126116225+lcastillo1486@users.noreply.github.com>
2023-03-23 22:54:00 -0500

    adminupdt

--- nuevaReserva/admin.py
+++ nuevaReserva/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import estadoReserva, nuevaReserva, estadoMesa, mesaNoo
+from .models import estadoReserva, nuevaReserva, estadoMesa, mesaNoo, mozosPlaza, anfitriona
 
 # Register your models here.
 
@@ -8,3 +8,5 @@ admin.site.register(estadoReserva)
 admin.site.register(nuevaReserva)
 admin.site.register(estadoMesa)
 admin.site.register(mesaNoo)
+admin.site.register(mozosPlaza)
+admin.site.register(anfitriona)
